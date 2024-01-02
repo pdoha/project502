a42ef42d34b8ed7bfbf5b6b6cba81e5e9703da08
pdoha <thdus5147@naver.com>
2024-01-02 17:45:56 +0900

    초기설정

---
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -0,0 +1,13 @@
+package org.choongang.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.build();
+    }
+}
